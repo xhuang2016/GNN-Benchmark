9b6a19964e290dc1cdb6a372f81807a3038dba54
Xin Huang <58825067+xhuang2016@users.noreply.github.com>
2022-09-12 13:29:05 -0500

    Add files via upload

---
+++ code/ClusterGCN/dgl_cluster_gcn-gpu-arxiv.py
@@ -0,0 +1,143 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl
+from ogb.nodeproppred import DglNodePropPredDataset
+from sklearn.metrics import f1_score
+from codecarbon import EmissionsTracker
+from energy_logger import energy_logger
+
+
+class ClusterGCN(nn.Module):
+    def __init__(self, in_feats, n_hidden, n_classes):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(dgl.nn.SAGEConv(in_feats, n_hidden, 'mean'))
+        # self.layers.append(dgl.nn.SAGEConv(n_hidden, n_hidden, 'mean'))
+        self.layers.append(dgl.nn.SAGEConv(n_hidden, n_classes, 'mean'))
+        self.dropout = nn.Dropout(0.0)
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+
+    def forward(self, sg, x):
+        h = x
+        for l, layer in enumerate(self.layers):
+            h = layer(sg, h)
+            if l != len(self.layers) - 1:
+                h = F.relu(h)
+                h = self.dropout(h)
+        # return h
+        return F.log_softmax(h, dim=-1)
+    
+    def inference(self, g, device, batch_size, num_workers, buffer_device=None):
+        feat = g.ndata['feat']
+        sampler = dgl.dataloading.MultiLayerFullNeighborSampler(1)
+        dataloader = dgl.dataloading.DataLoader(
+                g, torch.arange(g.num_nodes()).to(g.device), sampler, device=device,
+                batch_size=batch_size, shuffle=False, drop_last=False,
+                num_workers=num_workers)
+
+        if buffer_device is None:
+            buffer_device = device
+
+        for l, layer in enumerate(self.layers):
+            y = torch.empty(
+                g.num_nodes(), self.n_hidden if l != len(self.layers) - 1 else self.n_classes,
+                device=buffer_device, pin_memory=True)
+            feat = feat.to(device)
+            for _, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
+                x = feat[input_nodes]
+                h = layer(blocks[0], x)
+                if l != len(self.layers) - 1:
+                    h = F.relu(h)
+                    h = self.dropout(h)
+                y[output_nodes[0]:output_nodes[-1]+1] = h.to(buffer_device)
+            feat = y
+        return y
+
+
+def train():
+    model.train()
+    for sg in dataloader:
+        
+        sg = sg.to('cuda')
+        x = sg.ndata['feat']
+        y = sg.ndata['label']
+        m = sg.ndata['train_mask'].bool()
+        
+        y_hat = model(sg, x)
+        loss = F.cross_entropy(y_hat[m], y[m])
+        # loss_fc = nn.BCEWithLogitsLoss()
+        # loss = loss_fc(y_hat[m], y[m])
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+@torch.no_grad()
+def test():
+    model.eval()
+
+    m_train = graph.ndata['train_mask'].bool()
+    m_val = graph.ndata['val_mask'].bool()
+    m_test = graph.ndata['test_mask'].bool()
+    y_hat = model.inference(graph, device, 512, 0, 'cpu')
+
+    train_preds = y_hat[m_train]
+    train_labels = graph.ndata['label'][m_train]
+
+    val_preds = y_hat[m_val]
+    val_labels = graph.ndata['label'][m_val]
+
+    test_preds = y_hat[m_test]
+    test_labels = graph.ndata['label'][m_test]
+
+    train_preds = train_preds.argmax(dim=-1)
+    val_preds = val_preds.argmax(dim=-1)
+    test_preds = test_preds.argmax(dim=-1)
+    train_acc = f1_score(train_preds.cpu(), train_labels.cpu(), average='micro')
+    val_acc = f1_score(val_preds.cpu(), val_labels.cpu(), average='micro')
+    test_acc = f1_score(test_preds.cpu(), test_labels.cpu(), average='micro')
+    print('Training acc:', train_acc.item(),  'Validation acc:', val_acc.item(), 'Testing acc:', test_acc.item())
+
+
+log_name = 'dgl_cluster-ogbn-arxiv-cpugpu'
+tracker = EmissionsTracker(measure_power_secs=0.1, project_name=log_name, output_dir='log/', output_file='ClusterGCN-emissions.csv',)
+energy_logger(log_name)
+tracker.start()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+path = os.path.abspath(os.path.join(os.getcwd(), "../dataset/"))
+
+dataset = dgl.data.AsNodePredDataset(DglNodePropPredDataset('ogbn-arxiv', root=path))
+graph = dataset[0]
+graph = dgl.to_bidirected(graph,copy_ndata=True)
+
+model = ClusterGCN(graph.ndata['feat'].shape[1], 256, dataset.num_classes).cuda()
+opt = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0)
+
+num_partitions = 2000
+sampler = dgl.dataloading.ClusterGCNSampler(graph, num_partitions, cache_path='cache/cluster_gcn-arxiv.pkl')
+        # prefetch_ndata=['feat', 'label', 'train_mask', 'val_mask', 'test_mask'])
+dataloader = dgl.dataloading.DataLoader(
+        graph,
+        # torch.arange(num_partitions).to('cuda'),
+        torch.arange(num_partitions),
+        sampler,
+        # device='cuda',
+        device = 'cpu',
+        batch_size=50,
+        shuffle=True,
+        drop_last=False,
+        num_workers=0,
+        use_uva=False)
+
+
+print('DGL, ogbn-arxiv, ClusterGCN, cuda')
+for epoch in range(10):
+    train()
+print('Training done!')
+# test()
+# print('===============================')
+tracker.stop()
---
+++ code/ClusterGCN/pyg_cluster_gcn-cpu-reddit.py
@@ -0,0 +1,100 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.nn import ModuleList
+from torch_geometric.datasets import Reddit
+from torch_geometric.loader import ClusterData, ClusterLoader, NeighborSampler
+from torch_geometric.nn import SAGEConv
+from sklearn.metrics import f1_score
+from codecarbon import EmissionsTracker
+from energy_logger import energy_logger
+
+class ClusterGCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels, aggr='mean'))
+        self.convs.append(SAGEConv(hidden_channels, out_channels, aggr='mean'))
+        self.dropout = torch.nn.Dropout(p=0.0)
+
+    def forward(self, x, edge_index):
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
+            if i != len(self.convs) - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=0.0, training=self.training)
+        return F.log_softmax(x, dim=-1)
+
+    def inference(self, x_all):
+        for i, conv in enumerate(self.convs):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = conv((x, x_target), edge_index)
+                if i != len(self.convs) - 1:
+                    x = F.relu(x)
+                    x = self.dropout(x)
+                xs.append(x.cpu())
+                
+            x_all = torch.cat(xs, dim=0)
+
+        return x_all
+
+def train():
+    model.train()
+
+    for batch in train_loader:
+        batch = batch.to(device)
+        optimizer.zero_grad()
+        out = model(batch.x, batch.edge_index)
+        # loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
+        loss = F.cross_entropy(out[batch.train_mask], batch.y[batch.train_mask])
+        loss.backward()
+        optimizer.step()
+
+
+
+@torch.no_grad()
+def test():
+    model.eval()
+
+    out = model.inference(data.x)
+    y_pred = out.argmax(dim=-1)
+
+    accs = []
+    for mask in [data.train_mask, data.val_mask, data.test_mask]:
+        f1_micro = f1_score(data.y[mask], y_pred[mask], average='micro')
+        accs.append(f1_micro)  
+
+    print('Training acc:', accs[0],  'Validation acc:', accs[1], 'Testing acc:', accs[2])
+
+
+log_name = 'pyg_cluster-reddit-cpu'
+tracker = EmissionsTracker(measure_power_secs=0.1, project_name=log_name, output_dir='log/', output_file='ClusterGCN-emissions.csv',)
+energy_logger(log_name)
+tracker.start()
+
+device = torch.device('cpu')
+
+path = os.path.abspath(os.path.join(os.getcwd(), "../dataset/reddit/"))
+
+dataset = Reddit(path)
+data = dataset[0]
+
+cluster_data = ClusterData(data, num_parts=2000, recursive=False, save_dir='cache/')
+train_loader = ClusterLoader(cluster_data, batch_size=50, shuffle=True, num_workers=0)
+
+subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1], batch_size=1024, shuffle=False, num_workers=0)
+
+model = ClusterGCN(dataset.num_features, 256, dataset.num_classes).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+print('PyG, reddit, ClusterGCN, cpu')
+for epoch in range(10):
+    train()
+print('Training done!')
+# test()
+# print('===============================')
+tracker.stop()
